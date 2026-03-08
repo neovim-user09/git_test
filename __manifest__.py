b55f574b9a9722bd7e52bf9927b5b96b7043731f
neovim-user09 <prasiddha.shiwakoti02@gmail.com>
2026-03-09 00:29:58 +0545

    Changes in the manifest

--- __manifest__.py
+++ __manifest__.py
@@ -7,8 +7,8 @@
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Smarten Technologies Pvt. Ltd.",
+    'website': "https://www.smarten.com.np",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
