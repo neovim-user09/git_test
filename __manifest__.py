3f1123413ea327e13e7d8d1c708d23aa66c52d81
neovim-user09 <prasiddha.shiwakoti02@gmail.com>
2026-03-08 23:55:03 +0545

    Added logic for expiration date

--- __manifest__.py
+++ __manifest__.py
@@ -20,14 +20,9 @@ Long description of module's purpose
     'depends': ['base'],
 
     # always loaded
+    'depends': ['base','stock', 'sale_stock'],
+
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
-
